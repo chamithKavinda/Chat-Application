58aa09f2e2ebda31136f74f488fec651fbe1527a
chamithKavinda <chamith13kavinda@gmail.com>
2024-01-15 00:59:36 +0530

    Client controller class , launch and launchWrapper implemented

--- src/main/java/lk/ijse/LauncherClientWrapper.java
+++ src/main/java/lk/ijse/LauncherClientWrapper.java
@@ -1,4 +1,7 @@
 package lk.ijse;
 
 public class LauncherClientWrapper {
+    public static void main(String[] args) {
+        LauncherClient.main(args);
+    }
 }
--- src/main/java/lk/ijse/controller/ClientForm.java
+++ src/main/java/lk/ijse/controller/ClientForm.java
@@ -1,9 +1,16 @@
 package lk.ijse.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ClientForm {
 
@@ -13,9 +20,40 @@ public class ClientForm {
     @FXML
     private TextField txtClientField;
 
-    @FXML
-    void btnSendOnAction(ActionEvent event) {
+    ServerSocket serverSocket;
+    Socket socket;
 
+    DataInputStream inputStream;
+    DataOutputStream outputStream;
+
+    String message="";
+    String reply="";
+
+    public void initialize(){
+        new Thread(()->{
+            try {
+                socket = new Socket("localhost",3030);
+
+                while (!message.equals("finish")){
+                    inputStream = new DataInputStream(socket.getInputStream());
+                    outputStream = new DataOutputStream(socket.getOutputStream());
+                    message = inputStream.readUTF();
+
+                    Platform.runLater(()->{
+                        txtClientArea.setText(txtClientArea.getText()+"\n"+message);
+                    });
+                    System.out.println(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    @FXML
+    void btnSendOnAction(ActionEvent event) throws IOException {
+        outputStream.writeUTF(txtClientArea.getText());
+        outputStream.flush();
     }
 
 }
